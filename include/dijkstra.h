c399b0447cd35994267addc60283ca36f6e7ec38
mahmoodana1 <arabg1999@gmail.com>
2026-04-26 20:04:03 +0300

    initial change

--- include/dijkstra.h
+++ include/dijkstra.h
@@ -4,6 +4,6 @@
 #include "graph.h"
 
 // calculate shortest path using Dijkstra's algorithm
-void solve_dijkstra(Graph* g, int start, int end);
+void solveDijkstra(Graph* g, int start, int end);
 
 #endif
---
+++ src/dijkstra.c
@@ -0,0 +1,10 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "../include/graph.h"
+
+void solveDijkstra(Graph* g, int start, int end) {
+
+}
+
+#endif
