9d2729b3d49364df88dd014d6c40a695f21fb4f3
Loay Nimer <loeinimer21@gmail.com>
2026-04-27 19:42:14 +0300

    Final Dijkstra implementation and fixe louis

--- include/dijkstra.h
+++ include/dijkstra.h
@@ -5,4 +5,8 @@
 
 // calculate shortest path using Dijkstra's algorithm
 void solveDijkstra(Graph* g, int start, int end);
+int getMinDst(int dist[], int visited[], int n);
+void printPath(int parent[], int v);
+void printDijkstraResult(int parent[], int dst[], int start, int end) ;
+
 #endif
