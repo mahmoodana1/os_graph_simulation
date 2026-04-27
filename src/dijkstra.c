038dde7fd55813b1dfab0c68c171c2af593be699
Loay Nimer <loeinimer21@gmail.com>
2026-04-27 11:38:37 +0300

    Fix Dijkstra implementation

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -1,19 +1,22 @@
 #ifndef FILE_H
 #define FILE_H
-#define INF 1000000
-#include <stdio.h>
 
+#define INF 1000000
+
+#include <stdio.h>
 #include "../include/graph.h"
 
 static int getMinDst(int dist[], int visited[], int n) {
     int min = INF;
     int minIndex = -1;
+
     for (int i = 0; i < n; i++) {
         if (dist[i] < min && visited[i] == 0) {
             min = dist[i];
             minIndex = i;
         }
     }
+
     return minIndex;
 }
 
@@ -30,44 +33,54 @@ static void printPath(int parent[], int v) {
 void solveDijkstra(Graph *g, int start, int end) {
     if (start == end) {
         printf("%d\n", start);
-        printf("%d\n", end);
         return;
     }
+
     int numofVertises = g->num_nodes;
     int dst[numofVertises];
     int visited[numofVertises];
     int parent[numofVertises];
+
     for (int i = 0; i < numofVertises; i++) {
         dst[i] = INF;
         visited[i] = 0;
         parent[i] = -1;
     }
+
     dst[start] = 0;
+
     for (int i = 0; i < numofVertises; i++) {
         int temp = getMinDst(dst, visited, numofVertises);
+
         if (temp == -1) {
             break;
         }
+
         visited[temp] = 1;
+
         Node *cuurrent = g->adj[temp];
+
         while (cuurrent != NULL) {
             int v = cuurrent->id;
             int w = cuurrent->weight;
-            if (visited[v] == 0 && dst[temp] + w < dst[v]) { // RELAXATION
+
+            if (visited[v] == 0 && dst[temp] + w < dst[v]) {
                 dst[v] = dst[temp] + w;
                 parent[v] = temp;
             }
+
             cuurrent = cuurrent->next;
         }
     }
+
     if (dst[end] == INF) {
-        printf("No path found");
+        printf("No path found\n");
         return;
     }
 
     printPath(parent, end);
     printf("\n");
-
     printf("%d\n", dst[end]);
 }
+
 #endif
