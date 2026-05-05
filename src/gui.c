299ed52f11790f5b461c9ef7ecc1e579850b12ef
Ahmad Salman <ahmadsal2650@gmail.com>
2026-05-05 08:07:25 +0300

    feat: implement DrawRoad and DrawNodeTile functions

--- src/gui.c
+++ src/gui.c
@@ -26,6 +26,22 @@ static void DrawArrow(Vector2 start, Vector2 end, Color c) {
     Vector2 base = { tip.x - unit.x * aLen, tip.y - unit.y * aLen };
     DrawTriangle(tip, (Vector2){base.x + unit.y * aHalf, base.y - unit.x * aHalf}, (Vector2){base.x - unit.y * aHalf, base.y + unit.x * aHalf}, c);
 }
+
+static void DrawRoad(Vector2 a, Vector2 b, float thick, Color c) {
+    DrawLineEx(a, b, thick, c);
+    DrawCircleV(a, thick * 0.5f, c);
+    DrawCircleV(b, thick * 0.5f, c);
+    DrawArrow(a, b, c);
+}
+
+static void DrawNodeTile(RenderCtx* ctx, int idx) {
+    Vector2 p = ctx->positions[idx];
+    Rectangle r = { p.x - NODE_SIZE/2, p.y - NODE_SIZE/2, NODE_SIZE, NODE_SIZE };
+    DrawRectangleRounded(r, 0.25f, 8, MM_NODE_BG);
+    Color c = (idx == ctx->src) ? (Color){210, 80, 70, 255} : (idx == ctx->dst) ? (Color){60, 155, 155, 255} : ctx->accents[idx % 4];
+    DrawRectangleRounded((Rectangle){ r.x, r.y, NODE_SIZE, NODE_SIZE * 0.5f }, 0.25f, 6, c);
+    DrawText(TextFormat("%d", idx), (int)(p.x - 5), (int)(p.y + 8), 14, MM_TEXT_DARK);
+}
 /// /// /// //// /// / / // /  /
 
 
