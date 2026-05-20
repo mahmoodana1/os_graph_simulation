15cd8fbf89301d8fe9bd0d6a32d39a0b16acbdf5
mahmoodana1 <arabg1999@gmail.com>
2026-05-20 13:24:50 +0300

    solved rebaes conflict in graph.h

--- include/graph.h
+++ include/graph.h
@@ -6,22 +6,21 @@
 
 // node for adjacency list
 typedef struct Node {
-  int id;
-  int weight;
-  struct Node *next;
+    int id;
+    int weight;
+    struct Node *next;
 } Node;
 
 typedef struct {
-  Node *adj[MAX_NODES];
-  int num_nodes;
+    Node *adj[MAX_NODES];
+    int num_nodes;
 } Graph;
 
 // load data from file and create graph
-Graph* loadGraph(const char* file, int* start, int* end);
+Graph *loadGraph(const char *file, int *start, int *end);
 
-////
-Graph* createGraph(int numNodes);
-void addEdge(Graph* graph, int src, int dst, int weight);
-/////
-void freeAll(Graph* g);
+Graph *createGraph(int numNodes);
+void addEdge(Graph *graph, int src, int dst, int weight);
+void freeAll(Graph *g);
+
 #endif
