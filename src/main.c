db0c87dc584d5122de010f4649f9bfa51c491c24
Loay Nimer <loeinimer21@gmail.com>
2026-05-20 15:55:41 +0300

    Prepare main for traveler proces logic

--- src/main.c
+++ src/main.c
@@ -2,5 +2,9 @@
 #include "../include/graph.h"
 #include "../include/gui.h"
 #include <stdio.h>
-
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
