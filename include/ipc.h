7540a778464779f86f6f48c5bee5bfcb14d9ea99
mahmoodana1 <arabg1999@gmail.com>
2026-06-10 14:59:19 +0300

    eddited the creatShm and write functions in IPC to include a bigger shared memory and write to it correctly

--- include/ipc.h
+++ include/ipc.h
@@ -11,20 +11,21 @@ typedef struct {
     int next_node;
     int total_hops;
     volatile int queued_at_node; // its set to target node while queued, and set
-                                 // to -1 when not queued
+                                 // // to -1 when not queued
     sem_t sem_ready_to_read;
     sem_t sem_ready_to_write;
 } TravelerMsg;
 
-extern TravelerMsg *shm_ptr;
+extern TravelerMsg *travelers_shm_ptr;
 extern int shm_id; // shared memory ID for cleanup
 extern pid_t main_pid;
 extern sem_t *node_locks; // points to sem_t[MAX_NODES] at start of SHM
+extern void *raw_shm;
 
 void cleanup(int);
 void detachShm();
 void createShm(const int travelers_count);
-void initTravelerMsg(TravelerMsg *msg, const int taraveler_count);
+void initSemaphores(TravelerMsg *msg, const int taraveler_count);
 void writeTravelerPathToSharedMemory(TravelerMsg *shared_mem,
                                      int traveler_index, PathResult result);
 void readTravelerMsgFromSharedMemory(TravelerMsg *shared_mem,
