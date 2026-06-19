b0b50a947b19983d796caf70c9471a17350ca476
AboHashemJCE <moody_ashhab@hotmail.com>
2026-06-19 21:05:48 +0300

    edit graph.h: declared new functions to calculate remaining path cost

--- include/graph.h
+++ include/graph.h
@@ -4,21 +4,23 @@
 #define MAX_NODES 15
 #include "utils.h"
 
+struct Car;
 // node for adjacency list
 typedef struct Node {
-    int id;
-    int weight;
-    struct Node *next;
+  int id;
+  int weight;
+  struct Node *next;
 } Node;
 
 typedef struct {
-    Node *adj[MAX_NODES];
-    int num_nodes;
+  Node *adj[MAX_NODES];
+  int num_nodes;
 } Graph;
 
 // load data from file and create graph
 Graph *createGraph(int numNodes);
 void addEdge(Graph *graph, int src, int dst, int weight);
 Graph *loadGraph(const char *file, TravelerList *travelers);
+int path_remaining_cost(Car *car, Graph *g);
 void freeAll(Graph *g);
 #endif
