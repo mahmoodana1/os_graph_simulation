f6850e6fbaf7b10a10f421cd8acd3447f0f56e2c
AboHashemJCE <moody_ashhab@hotmail.com>
2026-06-19 21:51:21 +0300

    edit scheduler.c: implemented return scheduler name function

--- src/scheduler.c
+++ src/scheduler.c
@@ -7,9 +7,13 @@ int pick_winner(Car **queued, int n, int target, Graph *g) { return 0; }
 int fcfs_pick(Car **q, int n) { return 0; }
 
 int sjf_pick(Car **q, int n, Graph *g) {
-    if (n <= 0)
-        return -1;
-    return 0;
+  if (n <= 0)
+    return -1;
+  return 0;
 }
 
-const char *scheduler_name(void) { return "!!!"; }
+const char *scheduler_name(void) {
+  if (g_scheduler == SCHED_FCFS)
+    return "FCFS";
+  return "SJF";
+}
